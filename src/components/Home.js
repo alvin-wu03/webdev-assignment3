1410287965bc6d1a7bdcd61033398d96a4a6e8e5
xchen <xchen601@gmail.com>
2023-10-28 16:35:13 -0400

    Revert "updated homepage"
    This reverts commit 9e5a6ef3b827eb066e5e8411cab1dc1da7480d78.

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ class Home extends Component {
         <br/>
         <Link to="/login">Login</Link>
         <br/>
-        <Link to="/credits">Credits</Link>
+        <Link to="/credits">Credits (to be implemented in the Assignment)</Link>
         <br/>
         <Link to="/debits">Debits (to be implemented in the Assignment)</Link>
         <br/><br/>
