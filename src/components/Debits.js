337abe80ba6831863a0ccb48017b4fd35d8fe661
DFLotus <deondraefrancis101@gmail.com>
2023-10-28 14:02:02 -0400

    Added form input fields

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -4,30 +4,38 @@ src/components/Debits.js
 The Debits component contains information for Debits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AccountBalance from './AccountBalance';
 
 const Debits = (props) => {
   const { debits, balance } = props;
 
   return (
-    <div style={{textAlign: 'center'}}>
+    <div style={{ textAlign: 'center' }}>
       <h1>Debits</h1>
-      <ul style={{listStyle: 'none', paddingLeft: 0, textalign: 'center'}}>
+      <ul style={{ listStyle: 'none', paddingLeft: 0, textalign: 'center' }}>
         {debits.map((debit) => (
-        <li key={debit.id}>
-          -
-          <div>
-            <p><strong>Description:</strong> {debit.description}</p>
-            <p><strong>Amount:</strong> ${parseFloat(debit.amount).toFixed(2)}</p>
-            <p><strong>Date:</strong> {new Date(debit.date).toISOString().split('T')[0]}</p>
-          </div>
-        </li>
+          <li key={debit.id}>
+            -
+            <div>
+              <p><strong>Description:</strong> {debit.description}</p>
+              <p><strong>Amount:</strong> ${parseFloat(debit.amount).toFixed(2)}</p>
+              <p><strong>Date:</strong> {new Date(debit.date).toISOString().split('T')[0]}</p>
+            </div>
+          </li>
         ))}
       </ul>
-      <br/>
-      <AccountBalance accountBalance={balance}/>
-      <br/>
+      <br />
+      <AccountBalance accountBalance={balance} />
+      <br />
+      <form>
+        <label> Description:</label>
+        <input type="text"></input>
+        <label> Amount:</label>
+        <input type="text"></input>
+        <button><p>Add Debit</p></button>
+
+      </form>
       <Link to="/">Return to Home</Link>
     </div>
   );
